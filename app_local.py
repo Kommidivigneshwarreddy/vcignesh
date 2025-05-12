250a1245835fe7fd48e218c94a797c611c541750
kommidi vigneshwar reddy <vigneshwar.k@testphy.com>
2025-05-12 10:22:35 +0530

    Update app_local.py

--- app_local.py
+++ app_local.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Personalized AI Assistant", page_icon="🤖", lay
 st.markdown("<h1 style='text-align: center;'>💬 Personalized AI Assistant</h1>", unsafe_allow_html=True)
 
 # --- API Key ---
-api_key = "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"  # move this to st.secrets or env in production
+api_key = "CHATGROQ_API_KEY"  # move this to st.secrets or env in production
 
 # --- Session State ---
 if "chat_history" not in st.session_state:
